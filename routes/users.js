69ae2157cfff8a7104a951db4a313f49545c0805
trieu <lhtrieu.ktpm0115@student.ctuet.edu.vn>
2019-10-24 11:10:21 +0700

    update user routes

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('user/examples/package', {title: 'tiếng anh căn bản'});
-});
-
-router.get('/partials/user/examples/package.hbs', function(){
-  res.render('partials/user/examples/package');
+  res.render('partials/user/examples/package', {title: 'tiếng anh căn bản'});
 });
 
 module.exports = router;
