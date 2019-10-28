5fc8a87dfc41ace7e825b4553010627be446ed56
Roger Le <lengoctienthanh@gmail.com>
2019-10-28 10:45:10 +0700

    config DB

--- app.js
+++ app.js
@@ -3,44 +3,56 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const conn = require('./models/api/db');
+var methodOverride = require("method-override");
+var expressHbs = require('express-handlebars');
 
-
-
+// Include router
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
-app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.json());
+app.use('/', usersRouter);
 
-let routes = require('./models/api/routes') //importing route
-routes(app)
-
-
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Connecting route to database
+app.use(function (req, res, next) {
+  req.conn = conn;
+  next();
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// Parsing body request
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+//
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+app.listen(3000, function () {
+  console.log('server listening on port 3000');
 });
 
 module.exports = app;
--- models/api/db.js
+++ models/api/db.js
@@ -1,15 +1,15 @@
-var mysql      = require('mysql');
+var mysql = require('mysql');
 var db = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'duyvidai123',
-  database : 'web_tu_thien'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'web_tu_thien'
 });
-db.connect(function(err){
-if(!err) {
-    console.log("Database is connected");
-} else {
-    console.log("Error while connecting with database");
-}
+db.connect(function (err) {
+    if (!err) {
+        console.log("Database is connected");
+    } else {
+        console.log("Error while connecting with database");
+    }
 });
 module.exports = db; 
